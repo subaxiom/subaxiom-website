fb84d5a629e27fe182ab13ee167f45e88bc571ef
subaxiom <67562348+subaxiom@users.noreply.github.com>
2020-06-29 20:20:35 -0500

    changed title

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello Subaxiom3</h1>
+      <h1>Hello Subaxiom4</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
