39a4e15cdc6c827c5245e515c328f2c5544bcf48
subaxiom <67562348+subaxiom@users.noreply.github.com>
2021-11-04 19:11:32 -0500

    GitHub integration test desc 1 2 3

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import minds from "./img/minds-100x100.png";
 import medium from "./img/medium-100x100.png";
 import publish0x from "./img/publish0x-100x100.png";
 import youtube from "./img/youtube-100x100.png";
-import lbry from "./img/lbry-100x100.png";
 
 export default function App() {
   return (
@@ -19,8 +18,7 @@ export default function App() {
         <img src={minds} alt="minds" className="social" />
         <img src={medium} alt="medium" className="social" />
         <img src={publish0x} alt="publish0x" className="social" />
-        <img src={youtube} alt="youtube" className="social" />
-        <img src={lbry} alt="lbry" className="social" />
+        <img src={youtube} alt="youtuber3" className="social" />
       </div>
     </div>
   );
