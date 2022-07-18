d630607f730100fbe27b10959245a94b463a4b83
subaxiom <67562348+subaxiom@users.noreply.github.com>
2022-07-17 20:19:43 -0500

    Added arweave preview URLs for remaining images

--- src/galleryData.js
+++ src/galleryData.js
@@ -1,5 +1,289 @@
 const galleryMap = new Map();
 
+galleryMap.set("0050", {
+  src:
+    'url("https://noozavlnmhohizaq5fs363wzzodkmyiibonruu5u55pqapxcei.arweave.net/a52QV_W1h3HRkEOllv27Zy4amYQgLmxpTtO9fAD7iIo")',
+  title: "Liberty Toppled",
+  tags: new Set().add("joe biden").add("tony fauci"),
+  relevancy: 0
+});
+galleryMap.set("0049", {
+  src:
+    'url("https://4ocrnhyvvdl4x47ggnfl6vmdrod4beuegqinflpn2zsgf5xkcm.arweave.net/44UWnxWo1_8vz5jNKv1WDi4fAkoQ0ENKt7dZkYvbqEw")',
+  title: "Psycho Whitmer",
+  tags: new Set().add("joe biden").add("tony fauci"),
+  relevancy: 0
+});
+galleryMap.set("0048", {
+  src:
+    'url("https://jbz2webek3bz4qbqnaiqf66bfbtugvc5lwunpzcofrnw6by.arweave.net/SHOrECRWw55AMGgR-AvvBKGdDV-F1-dqNfk_Tixbbwc")',
+  title: "Capitol Hill Fly Trap",
+  tags: new Set().add("joe biden").add("tony fauci"),
+  relevancy: 0
+});
+galleryMap.set("0047", {
+  src:
+    'url("https://eylxg6eit6pcsyfhk4si3gbsvvbnbzwl6igjpytlyjs7ghvfdtcq.arweave.net/JhdzeIifnilgp1ckjZgyrULQ5svyDJfia8Jl8x6lHMU")',
+  title: "The Jan 6 Shaman",
+  tags: new Set().add("joe biden").add("tony fauci"),
+  relevancy: 0
+});
+galleryMap.set("0046", {
+  src:
+    'url("https://h2rb6sezqpvrxa6jio25fmazybjbxdmeyekyrx5wba3gfgym.arweave.net/PqIfSJmD6xuDyUO10rA_ZwFIbjYTB_FYjftgg2Y_psM")',
+  title: "Obey Gavin Newsom",
+  tags: new Set().add("joe biden").add("tony fauci"),
+  relevancy: 0
+});
+galleryMap.set("0045", {
+  src:
+    'url("https://rjm6blitnfjznga2qoqwytdcgo4zttvsturdzwvs3mtrtlxh5mya.arweave.net/ilngrRNpU5aYGoOhbExiM7mZzrKdIjzastsnGa7n6zA")',
+  title: "Our Puppet President",
+  tags: new Set().add("joe biden").add("tony fauci"),
+  relevancy: 0
+});
+galleryMap.set("0044", {
+  src:
+    'url("https://bxqmfytpecpsiia6udv47fiqmwujlao3ggjywczv74oc52jum4.arweave.net/_DeDC4m8gnyQgHqDrz5UQZaiVgdsxk4sLNf8cLuk0Z4")',
+  title: "Biden the Puppet",
+  tags: new Set().add("joe biden").add("tony fauci"),
+  relevancy: 0
+});
+galleryMap.set("0043", {
+  src:
+    'url("https://6ice2bmbui4pwjuwkrj6k2pq4actzpf3dzgyy4f2guhkdmma.arweave.net/8gRNBY--GiOPsmllRT-5Wnw4AU8vLseTYxwujUOobGA")',
+  title: "The Monkey Pox Gang",
+  tags: new Set().add("joe biden").add("tony fauci"),
+  relevancy: 0
+});
+galleryMap.set("0042", {
+  src:
+    'url("https://zvs2nm6jglpnuhkjqapxtoopccyfoxk35j42gu5fer2qcvn2.arweave.net/zWWms8ky3todSYAfebn_PELBXXVv_qeaNTpSR1AVW6M")',
+  title: "Frog in Boiling Water",
+  tags: new Set().add("joe biden").add("matrix").add("guns").add("crypto"),
+  relevancy: 0
+});
+galleryMap.set("0041", {
+  src:
+    'url("https://whbshufv5olzmb6zvwyf6okcvlhaapq6yk65stznfprkwi66ea.arweave.net/scMj0LXrl5YH2a2wXzlCqs4APh7CvdlPLSviqyP_eIA")',
+  title: "Yuval Noah Strangelove",
+  tags: new Set().add("joe biden").add("matrix").add("guns").add("crypto"),
+  relevancy: 0
+});
+
+galleryMap.set("0040", {
+  src:
+    'url("https://v5t7tgcbv4fdxaazumyqb7qrcec2xeuy65kjc6hty6tiqra3cq.arweave.net/r2f5mEGvCjuAGaMxAP4REQWrkpj3VJF488emiEQb-FA")',
+  title: "Bezos Seeks the Ring",
+  tags: new Set().add("joe biden").add("tony fauci"),
+  relevancy: 0
+});
+galleryMap.set("0039", {
+  src:
+    'url("https://nxwjxkjgwtz2vdcismvatlnuoa4u5hrtayqsniqwivn5wmtmpmgq.arweave.net/beybqSa086qMSJMqCa20cDlOnjMGISaiFkVb2zJsew0")',
+  title: "Mark of the Beast",
+  tags: new Set().add("joe biden").add("tony fauci"),
+  relevancy: 0
+});
+galleryMap.set("0038", {
+  src:
+    'url("https://4tfdeahcdvflmjz6bz2h27wlqkrfknf7kngidbi4nju36rt4.arweave.net/5MoyAOIdSrYnPg50fX7LgqJVNL9TTIGFHGpp-v0-Z8A")',
+  title: "Demon Lori Lightfoot",
+  tags: new Set().add("joe biden").add("tony fauci"),
+  relevancy: 0
+});
+galleryMap.set("0037", {
+  src:
+    'url("https://ephhgshpbfreiyivkblcypi2tu3rly54g4u6s4ady34jvtrt.arweave.net/I85z-SO-8JYkRhFVBWLD0anTcV47w3KelwA8b4ms4zo")',
+  title: "Hillary the Hutt",
+  tags: new Set().add("joe biden").add("tony fauci"),
+  relevancy: 0
+});
+galleryMap.set("0036", {
+  src:
+    'url("https://yqpgbjxvqdxc4nu4kqca7sd4wiknge4qhtst5qtmulnrv2ba.arweave.net/xB5gpvWA7i42nF_QED8h8shTTE5A85_T7CbKLbGuggE")',
+  title: "The Great Reset",
+  tags: new Set().add("joe biden").add("tony fauci"),
+  relevancy: 0
+});
+galleryMap.set("0035", {
+  src:
+    'url("https://w52wwziw2xinbeuo7gtkg24ibbcs2zedj7zkzy44mq2ljactmy.arweave.net/t3VrZRbV0NCSjvmmo2uICEUt-ZINP8qzjnGQ0tIBTZg")',
+  title: "Sheep Seek Safety",
+  tags: new Set().add("joe biden").add("tony fauci"),
+  relevancy: 0
+});
+galleryMap.set("0034", {
+  src:
+    'url("https://6mksq3unfyzsgjezpndluqruza7ovrqwqekeryukmyfdszjooe.arweave.net/8xUobo0uMy-MkmXtGukI0yD7qxhaBFEjiimYKOWUucU")',
+  title: "Wheelbarrow of Cash",
+  tags: new Set().add("joe biden").add("tony fauci"),
+  relevancy: 0
+});
+galleryMap.set("0033", {
+  src:
+    'url("https://smolhdvmg5cxabri2k2lvbyppdlnq6elfdc6tqjq24tzoomdbq.arweave.net/kxyzjqw3RXAGKNK0uocPeNbYeIsoxe_nBMNcnlzmDDI")',
+  title: "George Soros the Pied Piper",
+  tags: new Set().add("joe biden").add("tony fauci"),
+  relevancy: 0
+});
+galleryMap.set("0032", {
+  src:
+    'url("https://mircjwjucqynkzu7y3fexflm6jg27ejtbczk3zkchnd6yzpf.arweave.net/YiIk2TQUM-NVmn8bKS5Vs8k2vkTM-Isq3lQjtH7GXlI")',
+  title: "Disinformation Board",
+  tags: new Set().add("joe biden").add("matrix").add("guns").add("crypto"),
+  relevancy: 0
+});
+galleryMap.set("0031", {
+  src:
+    'url("https://2dbfxhmcrbko6hcyb4ehzlegpxfdeqpte5ia4dfnxmhalx3hfu.arweave.net/0MJbnYKIVO8cWA8IfKyGfcoyQ-fMnUA4MrbsOBd9nLQ")',
+  title: "Kamala In Headlights",
+  tags: new Set().add("joe biden").add("matrix").add("guns").add("crypto"),
+  relevancy: 0
+});
+
+galleryMap.set("0030", {
+  src:
+    'url("https://evotjohepo7iq6y3rdlqdgdn4u25lpk63fhhcolxggxwidar3i.arweave.net/JV00uOR7voh7G4jXA-Zht5TXVvV7ZTnE5dzGvZAwR2g")',
+  title: "Green New Deal",
+  tags: new Set().add("joe biden").add("tony fauci"),
+  relevancy: 0
+});
+galleryMap.set("0029", {
+  src:
+    'url("https://jc3smajzsdpsnv2ld2o6aar2fesqoj3mwzbpgqqhu2m2buej.arweave.net/SLcmAT_mQ3ybXSx6d4AI6KSUHJ2y2Qv-NCB6aZoNCJ0")',
+  title: "Illuminati",
+  tags: new Set().add("joe biden").add("tony fauci"),
+  relevancy: 0
+});
+galleryMap.set("0028", {
+  src:
+    'url("https://auxfrx4a4iqedajnrytxgezyokiga6v5j2mgqmgqeiqcr4epysra.arweave.net/BS5Y34DiIEGBLY4ncxM4cpBger1OmGgw0CIgKPCPxKI")',
+  title: "Real World Agent Smiths",
+  tags: new Set().add("joe biden").add("tony fauci"),
+  relevancy: 0
+});
+galleryMap.set("0027", {
+  src:
+    'url("https://k3f5v4j3owpq3znwg76iv64ckt5ldwiotchyupkgja6xxu6tua.arweave.net/Vsva8Tt1nw3ltjf8ivuCVPqx2Q6Yj4o9R-kg9e9PToI")',
+  title: "Praying for Oil",
+  tags: new Set().add("joe biden").add("tony fauci"),
+  relevancy: 0
+});
+galleryMap.set("0026", {
+  src:
+    'url("https://o3l7i4qcatd2a6l55nndm53rf5feulngw5f3aayzs6zqwoiuy4.arweave.net/dtf0cgIEx6B5-fetaNndxL0pKLaa3S7ADGZezCzkUx0")',
+  title: "Duck & Cover & Mask",
+  tags: new Set().add("joe biden").add("tony fauci"),
+  relevancy: 0
+});
+galleryMap.set("0025", {
+  src:
+    'url("https://nguzipwkpvdritr3c2xuabpou7hgq4suzcy7ezmim7hdssc6uy3a.arweave.net/aamUPsp9RxROOxavQAXup85oclTIsfJliGfOOUhepjY")',
+  title: "Rem Die Severe",
+  tags: new Set().add("joe biden").add("tony fauci"),
+  relevancy: 0
+});
+galleryMap.set("0024", {
+  src:
+    'url("https://npcryftiaba2kpg3qqrbc7b3ajocpoxfndpstdbstbqwtmacd4.arweave.net/a8UcFmgAQaU824-QiEXw7AlwnuuVo3ymMMphhabACH0")',
+  title: "You Are A Racist",
+  tags: new Set().add("joe biden").add("tony fauci"),
+  relevancy: 0
+});
+galleryMap.set("0023", {
+  src:
+    'url("https://c4voizer7mlwwydlh5oujth5l3sunf4mitx2qvksz3lnvz3y.arweave.net/_FyrkZJH7F2tgaz9dRMz9XuVGl4xE-76hVUs7W2u-d4")',
+  title: "Mask Nazis",
+  tags: new Set().add("joe biden").add("tony fauci"),
+  relevancy: 0
+});
+galleryMap.set("0022", {
+  src:
+    'url("https://b7gngyhj56yow2xjeuyuqheociofifemti5tgmyey44t3ncvu34q.arweave.net/D8zTYOnvsOtq6SUxSByOEhxUFIyaOzMzBMc5PbRVpvk")',
+  title: "Trudeau Castro",
+  tags: new Set().add("joe biden").add("matrix").add("guns").add("crypto"),
+  relevancy: 0
+});
+galleryMap.set("0021", {
+  src:
+    'url("https://izsnnnkvhbilqccgyimj5vrbh2yymnh4w6i3lknutd2t44gqfe.arweave.net/RmTWt-VU4ULgIRsIYntYhPrGGNPy3kbWptJj1PnDQKU")',
+  title: "Pelosi Attacks",
+  tags: new Set().add("joe biden").add("matrix").add("guns").add("crypto"),
+  relevancy: 0
+});
+
+galleryMap.set("0020", {
+  src:
+    'url("https://sk2jims5kblb4gbksfxzmkycyhss7w567qdwyki5kis5b5hr.arweave.net/krSUMl1QVh-4YKpFvlisCweUv2778B2wpHVIl0PT_xA")',
+  title: "Chuck Schumer Swamp Creature",
+  tags: new Set().add("joe biden").add("tony fauci"),
+  relevancy: 0
+});
+galleryMap.set("0019", {
+  src:
+    'url("https://wcc5rrho2sfflbo6rpk2hq2wiicf7uspajozkrznj2fnuxia3e.arweave.net/sIXYxO7UilWF3ovVo8NWQgRf0k8CXZVHLU-6K2l0A2Q")',
+  title: "Kool Aid Mandatory",
+  tags: new Set().add("joe biden").add("tony fauci"),
+  relevancy: 0
+});
+galleryMap.set("0018", {
+  src:
+    'url("https://gdddzummetmjmdxqknl6ensfgeyd3wuhzvfkqpe2tcayk.arweave.net/MMY80Y_wk2JYO8FNX4jZFMTA_92ofN_Sqg8m_p-iBhU")',
+  title: "Tucker Tank Man",
+  tags: new Set().add("joe biden").add("tony fauci"),
+  relevancy: 0
+});
+galleryMap.set("0017", {
+  src:
+    'url("https://aawm6g46vk7d23fqqzmia52bdutjpwgyty3xycuhlswfrboutu.arweave.net/ACzPG56qv-j1ssIZYgHdBHSaX2NieN3wKh1ysWIXUnY")',
+  title: "Fauci The Exterminator",
+  tags: new Set().add("joe biden").add("tony fauci"),
+  relevancy: 0
+});
+galleryMap.set("0016", {
+  src:
+    'url("https://2e4z5k463ithnzgukt47cikqitb3hyujd3lt52zusupemg6pt7nq.arweave.net/0Tmeq57aJnbk1FT58SFQRMOz4oke1z7rNJUeRhvPn9s")',
+  title: "Malone Knocks Out Fauci",
+  tags: new Set().add("joe biden").add("tony fauci"),
+  relevancy: 0
+});
+galleryMap.set("0015", {
+  src:
+    'url("https://h4ujyy223aalynrc7onx5dcefdv5lzz52ynah5c7jguqgxdn5voq.arweave.net/PyicY1rYALw2IvubfoxEKOvV5z3WGgP0X0mpA1xt7V0")',
+  title: "Klaus Schwab Hellraiser",
+  tags: new Set().add("joe biden").add("tony fauci"),
+  relevancy: 0
+});
+galleryMap.set("0014", {
+  src:
+    'url("https://nogxq72rchgki22xrigfuklbs6zabxut3svpv4zjyxiackca.arweave.net/a414f1ERzKRrV4oMWilhl7IA3pPcqvrzK_cX-QAShAQ")',
+  title: "Alex Jones Burning Monk",
+  tags: new Set().add("joe biden").add("tony fauci"),
+  relevancy: 0
+});
+galleryMap.set("0013", {
+  src:
+    'url("https://dyrfbr2lsbarfwvitigmrnntdbvqpdagqq4mvd2slihhc5vptq.arweave.net/_HiJQx0uQQRLaqJoMyLWzGGsHjAaEOMqPUloOcXavnE")',
+  title: "Joe Rogan as King Kong",
+  tags: new Set().add("joe biden").add("tony fauci"),
+  relevancy: 0
+});
+galleryMap.set("0012", {
+  src:
+    'url("https://uu4pqmnavin3fz5gr43kdfw2dnkn6ylbedn3wmjryqkhdg7v.arweave.net/pTj4_MaCqG7Lnpo82oZ_baG1TfYWEg27sxMcQUcZv1g")',
+  title: "Metaverse Mark",
+  tags: new Set().add("joe biden").add("matrix").add("guns").add("crypto"),
+  relevancy: 0
+});
+galleryMap.set("0011", {
+  src:
+    'url("https://2x4zyfnbmr6zbbpwsbksbeqjtsimffjunsck2wwpqvgydrnu.arweave.net/1fmcFaFkfZCF9pBVIJIJnJ_DClTRshK1az4VN_gcW00")',
+  title: "V Places Flower in Syringe",
+  tags: new Set().add("joe biden").add("matrix").add("guns").add("crypto"),
+  relevancy: 0
+});
+
 galleryMap.set("0010", {
   src:
     'url("https://it4kappv2zlfl2jjikevinxgkcirelgxd27eo4kthtwteai7.arweave.net/RPigPfXWVlXpKUKJVDbmUJESL_NcevkdxUzztMg_EfM")',
