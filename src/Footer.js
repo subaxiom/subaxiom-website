ac4eca604b0635e655eecbcee3da1105aaddc6d7
subaxiom <67562348+subaxiom@users.noreply.github.com>
2022-11-02 20:59:53 -0500

    added Contact Us email section to footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,34 +1,47 @@
 import React from "react";
 import socialSprite from "./img/socialSprite.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 //import { useHistory } from "react-router";
 
 export const Footer = () => {
   return (
     <div className="footer">
-      <h3>
-        connect on social: <b>@subAxiom</b>
-      </h3>
-      <table>
-        <SocialRow spritePos="0px" socialUrl="https://twitter.com/subAxiom" />
-        <SocialRow
-          spritePos="-20px"
-          socialUrl="https://gettr.com/user/subAxiom"
-        />
-        <SocialRow spritePos="-40px" socialUrl="https://gab.com/subAxiom" />
-        <SocialRow spritePos="-60px" socialUrl="https://parler.com/subAxiom" />
-        <SocialRow
-          spritePos="-80px"
-          socialUrl="https://truthsocial.com/@subAxiom"
-        />
-        <SocialRow
-          spritePos="-100px"
-          socialUrl="https://t.me/subAxiomChannel"
-        />
-        <SocialRow
-          spritePos="-120px"
-          socialUrl="https://subAxiom.substack.com"
-        />
-      </table>
+      <div className="footer-section">
+        <h2>contact us:</h2>
+        <div className="footer-email-link">
+          <a href="mailto:subAxiom@protonmail.com">
+            <FontAwesomeIcon icon={faEnvelope} /> subAxiom@protonmail.com
+          </a>
+        </div>
+      </div>
+      <div className="footer-section">
+        <h2>connect on social:</h2>
+        <table>
+          <SocialRow spritePos="0px" socialUrl="https://twitter.com/subAxiom" />
+          <SocialRow
+            spritePos="-20px"
+            socialUrl="https://gettr.com/user/subAxiom"
+          />
+          <SocialRow spritePos="-40px" socialUrl="https://gab.com/subAxiom" />
+          <SocialRow
+            spritePos="-60px"
+            socialUrl="https://parler.com/subAxiom"
+          />
+          <SocialRow
+            spritePos="-80px"
+            socialUrl="https://truthsocial.com/@subAxiom"
+          />
+          <SocialRow
+            spritePos="-100px"
+            socialUrl="https://t.me/subAxiomChannel"
+          />
+          <SocialRow
+            spritePos="-120px"
+            socialUrl="https://subAxiom.substack.com"
+          />
+        </table>
+      </div>
     </div>
   );
 };
