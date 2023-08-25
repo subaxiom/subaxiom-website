e84d74c13ad6539d296dd05edf44e7a1342a7454
subaxiom <67562348+subaxiom@users.noreply.github.com>
2023-08-25 14:26:11 -0500

    transforming to search engine

--- src/App.js
+++ src/App.js
@@ -12,6 +12,19 @@ export default function App(props) {
   let galleryData = props.galleryData;
   let scrollToVertical = props.scrollToVertical;
 
+  var searchResponse = "search results not found 2";
+  //let searchUrl = "https://149.28.123.181:8000/?query=Mr.%20Miyagi%20and%20Daniel%20fight%20Sensei%20Kreese%20and%20Johnny%20in%20a%20karate%20tournament%0A";
+  async function logMovies() {
+    //const response = await fetch("https://api.github.com/users/hadley/orgs");
+    const response = await fetch(
+      "https://api.subaxiom.com/?query=Mr.%20Miyagi%20and%20Daniel%20fight%20Sensei%20Kreese%20and%20Johnny%20in%20a%20karate%20tournament%0A"
+    );
+    const movies = await response.text();
+    alert(String(movies));
+    searchResponse = String(movies);
+  }
+  logMovies();
+
   return (
     <div className="App">
       <NavBar cartMap={props.cartMap} />
@@ -21,6 +34,7 @@ export default function App(props) {
         <Filter filter={filter} filterOrTagClicked={filterOrTagClicked} />
         <div className="page-wrapper">
           <Gallery
+            searchResponse={searchResponse}
             galleryData={galleryData}
             scrollToVertical={scrollToVertical}
           />
--- src/Gallery.js
+++ src/Gallery.js
@@ -12,6 +12,19 @@ const GalleryComponent = (props) => {
   let scrollToVertical = props.scrollToVertical;
   if (justSimilar) imageArray = galleryData.similar;
 
+  /*
+  
+    // API for get requests
+    let fetchRes = fetch(
+      "149.28.123.181:8000/?query=Mr.%20Miyagi%20and%20Daniel%20fight%20Sensei%20Kreese%20and%20Johnny%20in%20a%20karate%20tournament%0A", {method:'GET', mode:'cors'});
+      // fetchRes is the promise to resolve
+      // it by using.then() method
+      fetchRes.then(res =>
+          res.json()).then(d => {
+              alert(JSON.stringify(d))
+          })
+    */
+
   let thumbnailOnclick = function (imageId) {
     navigate(`/image/${imageId}`, { replace: true });
     scrollToVertical(0);
@@ -36,9 +49,11 @@ export const Gallery = (props) => {
   let galleryData = props.galleryData;
   let justSimilar = props.justSimilar;
   let scrollToVertical = props.scrollToVertical;
+  let searchResponse = props.searchResponse;
 
   return (
     <div>
+      <div>{searchResponse}</div>
       <GalleryComponent
         galleryData={galleryData}
         justSimilar={justSimilar}
